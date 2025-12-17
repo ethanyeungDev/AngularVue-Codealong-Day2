042225db21e83a799df50471a7663e2877fa31dd
ethanyeungDev <ethanyeungpg@gmail.com>
2025-12-17 11:53:11 -0800

    setup example 9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PageAComponent } from './app.page-a';
+import { PageBComponent } from './app.page-b';
+import { PageDefault } from './app.page-default';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'page-a', component: PageAComponent },
+    { path: 'page-b', component: PageBComponent },
+    { path: '', redirectTo: '/page-a', pathMatch: 'full' },
+    { path: '**', component: PageDefault }  
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,16 @@
-import { Component, Inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MyDataService } from './myDataService';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule],
-
-    // 'providers' allows you to create and pass an instance
-    // of the class to the constructor header.
-    providers: [MyDataService],
-    template: `<h1>Hello world!  {{title}}</h1>
-               <ul><li *ngFor="let name of names">{{name}}</li></ul>`
+    imports: [RouterOutlet],
+    template:   
+      `<h1>This is the header</h1>
+      <nav>
+        <a href="/page-a"> A </a> | <a href="/page-b"> B </a>
+      </nav>
+      <!-- Where router should display a view -->
+      <router-outlet></router-outlet>`
 })
-export class App {
-    public title = 'This is Angular!';
-    names: Array<any>;
-
-    // Create instance of 'MyDataService' right in the constructor header.
-    constructor(@Inject(MyDataService) myDataService: MyDataService) {
-        // Use service to call getNames() method.
-        this.names = myDataService.getNames();
-    }
-}
+export class App { }
